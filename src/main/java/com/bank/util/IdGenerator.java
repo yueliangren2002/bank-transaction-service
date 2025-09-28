4386caed48abf50a24129c07f5e5b46e596321fd
yueliangren2002 <157577782+yueliangren2002@users.noreply.github.com>
2025-09-28 17:24:15 +0800

    Update IdGenerator.java

--- src/main/java/com/bank/util/IdGenerator.java
+++ src/main/java/com/bank/util/IdGenerator.java
@@ -1,9 +1,9 @@
 package com.bank.util;
 
-import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
-    private static LongAdder counter = new LongAdder();
+    private static final AtomicLong counter = new AtomicLong(0);
     private static final int MAX_COUNTTER = 50000;
 
     /**
@@ -15,8 +15,7 @@ public class IdGenerator {
         // 获取当前时间戳
         long timestamp = System.currentTimeMillis();
         // 获取一个递增的计数器
-        counter.increment();
-        long uniqueId = counter.longValue() % MAX_COUNTTER;
+        long uniqueId = counter.incrementAndGet() % MAX_COUNTTER;
         // 组合时间戳和递增计数器生成唯一ID
         return timestamp * MAX_COUNTTER + uniqueId;
     }
